4c2bf477b863e4d2ad91c42c82e73f3d6d051589
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-02-14 00:26:57 -0600

    finish first practice

--- Practica_1/practica_1.py
+++ Practica_1/practica_1.py
@@ -1,13 +1,22 @@
 import cv2
+from time import sleep
 
 PHOTO_1 = r"Practica_1\foto_prueba.png"
 
-def adq_preprocess_photo(path, type):
+def adq_preprocess_photo(path, type='color', color='bgr'):
     type_dict = {'gray': cv2.IMREAD_REDUCED_GRAYSCALE_2,
             'color': cv2.IMREAD_REDUCED_COLOR_2,
             'unchanged': cv2.IMREAD_IGNORE_ORIENTATION}
     
-    img = cv2.imread(path, type_dict[type])
+    color_dict = {'bgr': cv2.COLOR_BGR2HSV,
+                  'gray': cv2.COLOR_BGR2GRAY}
+
+    if type_dict['color']:
+        src = cv2.imread(path, type_dict[type])
+        img = cv2.cvtColor(src, color_dict[color])
+    else:
+        img = cv2.imread(path, type_dict[type])
+
     try:
         cv2.imshow("image", img)
         cv2.waitKey(0)
@@ -15,4 +24,5 @@ def adq_preprocess_photo(path, type):
         pass
     cv2.destroyAllWindows()
 
-adq_preprocess_photo(PHOTO_1, 'color')
+
+adq_preprocess_photo(PHOTO_1, color='gray')
