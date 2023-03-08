c083cce66f15bf01e4fd7d6f6e2d42cbb0157d45
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-03-07 22:24:05 -0600

    rename folder

--- Clase/Actividad_2/traslacion.py
+++ Clase/Actividad_2/traslacion.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('Ave_2.jpg')
+image = cv2.imread('portada.jpg')
 ancho = image.shape[1] #columnas
 alto = image.shape[0] # filas
 
-TRAS_X = 100
-TRAS_Y = 250
+TRAS_X = 600
+TRAS_Y = 10
 
 # Traslación
 M = np.float32([[1,0,TRAS_X],[0,1,TRAS_Y]])
