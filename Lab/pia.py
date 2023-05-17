a38b0a65a25004322e648d9f5c9f85397fef00bd
Panda <emilianoamaro@hotmail.com>
2023-05-17 17:04:20 -0600

    make it work better

--- Lab/pia.py
+++ Lab/pia.py
@@ -10,12 +10,18 @@ def imshow(title = "Image", image = None, size = 10):
     plt.title(title)
     plt.show()
 
-def facesAlgorithm(gray, frane):
+def facesAlgorithm(gray, frame):
     face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(127,0,255),2)
 
+def bodyAlgorithm(gray, frame):
+    body_classifier = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+    bodies = body_classifier.detectMultiScale(gray,1.12, 5)
+    print(bodies)
+    for (x,y,w,h) in bodies:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
@@ -29,6 +35,7 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     facesAlgorithm(gray, frame)
+    bodyAlgorithm(gray, frame)
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
